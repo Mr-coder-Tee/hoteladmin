74d50151f67d3443f712913214162534694151eb
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-11-17 16:01:40 +0200

    front end

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import image from "../../Image/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEye,
+  faEyeSlash,
+  faAt,
+  faLock,
+} from "@fortawesome/free-solid-svg-icons";
+
+const LoginPage = () => {
+  const [type, setType] = useState("password");
+  const EyeChanger = () =>
+    type === "password" ? (
+      <FontAwesomeIcon icon={faEye} />
+    ) : (
+      <FontAwesomeIcon icon={faEyeSlash} />
+    );
+
+  const passwordVisibility = () => {
+    if (type === "password") {
+      setType("text");
+    } else {
+      setType("password");
+    }
+  };
+
+  return (
+    <div className="logInContainer">
+      <div className="imageContainer">
+        <img
+          src="https://images.unsplash.com/photo-1517840901100-8179e982acb7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+          alt="background image"
+          className="image"
+        />
+        <img src={image} className="logo" />
+      </div>
+      <div className="loginForm">
+        <div className="form">
+          <h2 style={{ textAlign: "center", marginBottom: 10, marginTop: 10,fontFamily:'monospace' ,fontSize:34}}>
+            Sign In
+          </h2>
+          <div className="inputContainer">
+            <FontAwesomeIcon icon={faAt} />
+            <input placeholder="Email" className="input" />
+          </div>
+          <div className="inputContainer">
+            <FontAwesomeIcon icon={faLock} />
+            <input placeholder="Password" className="input"  type={type}/>
+            <button  type="button" className="passwordBtn" onClick={()=>passwordVisibility()}>
+              <EyeChanger />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
